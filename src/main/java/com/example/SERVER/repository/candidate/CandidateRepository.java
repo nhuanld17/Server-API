bff2a84277f499d66b60cc1ea57b25b95a409b84
Le Dinh Nhuan <ledinhnhuan1917@gmail.com>
2024-12-22 05:35:49 +0700

    create api search by specification

--- src/main/java/com/example/SERVER/repository/candidate/CandidateRepository.java
+++ src/main/java/com/example/SERVER/repository/candidate/CandidateRepository.java
@@ -1,9 +1,12 @@
 package com.example.SERVER.repository.candidate;
 
 import com.example.SERVER.domain.entity.candidate.Candidate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+	Page<Candidate> findByFullNameContaining(String candidateName, Pageable pageable);
 }
--- src/main/java/com/example/SERVER/service/canditate/CandidateService.java
+++ src/main/java/com/example/SERVER/service/canditate/CandidateService.java
@@ -1,8 +1,13 @@
 package com.example.SERVER.service.canditate;
 
+import com.example.SERVER.domain.dto.candidate.CandidateIntroDTO;
+import com.example.SERVER.domain.dto.common.Meta;
+import com.example.SERVER.domain.dto.common.ResultPaginationDTO;
 import com.example.SERVER.domain.entity.candidate.Candidate;
 import com.example.SERVER.repository.candidate.CandidateRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
